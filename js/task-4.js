d75f7619557c3e0bcc74f3caae846fef46c8f09c
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-03-05 22:50:48 +0200

    done the 4 task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form')
+
+const form = {  
+}
+
+formEl.addEventListener('submit', formHandler)
+
+function formHandler(evt){
+    evt.preventDefault()
+    
+    if(!formEl.elements.email.value || !formEl.elements.password.value){
+        alert('All form fields must be filled in')
+        formEl.reset()
+        return
+    }
+
+    form.email = formEl.elements.email.value.trim();
+    form.password=formEl.elements.password.value.trim();
+    console.log(form)
+
+    formEl.reset()
+}
 
