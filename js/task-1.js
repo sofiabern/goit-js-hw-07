5db9a6128d5f872f0e65f9b4dc5ca64ce618048c
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-03-11 22:15:42 +0200

    styled task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,10 @@
-const ulEl = document.querySelector('#categories')
-// console.dir(ulEl)
-const children = [...ulEl.children]
-// console.log(children)
-console.log(`Number of categories: ${children.length}`)
+const categoriesList = document.querySelector('#categories');
+const children = [...categoriesList.children];
 
-children.forEach((item, idx, arr) =>{
-// console.log(arr[idx].children)
-const descendants = arr[idx].children
-// console.log(descendants)
-console.log(`Category: ${descendants[0].textContent}`)
-console.log(`Elements: ${descendants[1].children.length}`)
-})
+console.log(`Number of categories: ${children.length}`);
+
+children.forEach((item) => {
+  const descendants = item.children;
+  console.log(`Category: ${descendants[0].textContent}`);
+  console.log(`Elements: ${descendants[1].children.length}`);
+});
